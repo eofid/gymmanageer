e87fdc5906b4356d0c1e7ae32466088f48492cbc
eofid <arhipadynec@gmail.com>
2025-05-26 14:54:52 +0300

    lab8

--- src/test/java/com/gym/management/gymmanager/PersonServiceTest.java
+++ src/test/java/com/gym/management/gymmanager/PersonServiceTest.java
@@ -26,7 +26,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class PersonServiceTest {
+class PersonServiceTest {
 
     @InjectMocks
     private PersonService personService;
