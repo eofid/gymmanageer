8360dd4c5736a7c69efa518cb4bf17d3963d13c2
eofid <arhipadynec@gmail.com>
2025-05-26 14:33:10 +0300

    lab8

--- src/main/java/com/gym/management/gymmanager/service/GymService.java
+++ src/main/java/com/gym/management/gymmanager/service/GymService.java
@@ -4,12 +4,10 @@ import com.gym.management.gymmanager.model.Gym;
 import com.gym.management.gymmanager.repository.GymRepository;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GymService {
-    @Autowired
     private GymRepository gymRepository;
 
     // 1. Сохранение зала
