f35279fe8cf6811109015b3153066d3cedee59e3
eofid <arhipadynec@gmail.com>
2025-05-26 12:13:31 +0300

    lab8

--- src/main/java/com/gym/management/gymmanager/controller/GymController.java
+++ src/main/java/com/gym/management/gymmanager/controller/GymController.java
@@ -3,7 +3,6 @@ package com.gym.management.gymmanager.controller;
 import com.gym.management.gymmanager.model.Gym;
 import com.gym.management.gymmanager.service.GymService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
